361f99f762686ece1b5b3e078e8b0bccfcf4e61c
Sean Song <inferrinizzard@gmail.com>
2019-01-09 20:39:11 -0800

    Added Material-UI, css colours, app.tsx
    edited gitignore to ignore node_modules folder, roboto font in index.html

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export interface AppProps {
+	
+}
+ 
+export interface AppState {
+	
+}
+ 
+class App extends React.Component<AppProps, AppState> {
+	state = { :  }
+	render() { 
+		return (  );
+	}
+}
+ 
+export default App;
