77c46be40119900784a55df2118e3392bd3884b0
inferrinizzard <inferrinizzard@gmail.com>
2019-09-05 22:22:06 -0700

    update favicon

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -25,29 +25,25 @@ export interface MainDivProps {
   drawerWidth: number;
 }
 
-const MainDiv = styled.div`
+const MainDiv = styled.div<MainDivProps>`
   margin-top: ${(p: MainDivProps) => p.topLinerHeight}px;
-  margin-left: ${p => toPX(p.drawerWidth + 'em')}px;
+  margin-left: ${(p: MainDivProps) => toPX(p.drawerWidth + 'em')}px;
   width: calc(
-    ${p => (p.open ? 52 : 100)}% - ${p => toPX(p.drawerWidth + 'em') + 10}px
+    ${(p: MainDivProps) => (p.open ? 52 : 100)}% -
+      ${(p: MainDivProps) => toPX(p.drawerWidth + 'em') + 10}px
   );
   height: 100%;
 `;
 
-const Main: React.SFC<MainProps & MainDivProps> = props => {
-  const setActive = (course: Course, row: number) => {
-    props.openDetail(course, row);
-    props.scrollTo(row);
-  };
-  return (
-    <MainDiv
-      {...{
-        topLinerHeight: props.topLinerHeight,
-        drawerWidth: props.drawerWidth,
-        open: props.open,
-      }}
-    >
-      {/* <div
+const Main: React.SFC<MainProps & MainDivProps> = props => (
+  <MainDiv
+    {...{
+      topLinerHeight: props.topLinerHeight,
+      drawerWidth: props.drawerWidth,
+      open: props.open,
+    }}
+  >
+    {/* <div
         style={{
           marginTop: props.topLinerHeight + 'px',
           marginLeft: props.drawerWidth + 'em',
@@ -60,63 +56,65 @@ const Main: React.SFC<MainProps & MainDivProps> = props => {
           height: '100%',
         }}
       ></div> */}
-      <AutoSizer>
-        {({ height, width }: { height: number; width: number }) => {
-          const columns = Math.floor(width / toPX(props.cardWidth + 'em'));
-          const rows = Math.ceil(props.courses.length / columns);
-          return (
-            <div>
-              <List
-                width={width}
-                height={height}
-                rowCount={rows}
-                rowHeight={toPX(props.cardHeight + 'em')}
-                overscanRowCount={4}
-                scrollToAlignment={'start'}
-                scrollToIndex={props.scrollIndex}
-                style={{
-                  outline: 'none',
-                }}
-                rowRenderer={({
-                  index,
-                  key,
-                  style,
-                }: {
-                  index: number;
-                  key: string;
-                  style: any;
-                }) => {
-                  const items = [];
-                  const fromIndex: number = index * columns;
-                  const toIndex: number = Math.min(
-                    fromIndex + columns,
-                    props.courses.length
-                  );
+    <AutoSizer>
+      {({ height, width }: { height: number; width: number }) => {
+        const columns = Math.floor(width / toPX(props.cardWidth + 'em'));
+        const rows = Math.ceil(props.courses.length / columns);
+        return (
+          <div>
+            <List
+              width={width}
+              height={height}
+              rowCount={rows}
+              rowHeight={toPX(props.cardHeight + 'em')}
+              overscanRowCount={4}
+              scrollToAlignment={'start'}
+              scrollToIndex={props.scrollIndex}
+              style={{
+                outline: 'none',
+              }}
+              rowRenderer={({
+                index,
+                key,
+                style,
+              }: {
+                index: number;
+                key: string;
+                style: any;
+              }) => {
+                const items = [];
+                const fromIndex: number = index * columns;
+                const toIndex: number = Math.min(
+                  fromIndex + columns,
+                  props.courses.length
+                );
 
-                  // slice, maybe?
-                  for (let i = fromIndex; i < toIndex; i++)
-                    items.push(
-                      <ClassCard
-                        key={i}
-                        row={index}
-                        courseData={props.courses[i]}
-                        active={props.active}
-                        openDetail={setActive}
-                      />
-                    );
-                  return (
-                    <div key={key} style={style}>
-                      {items}
-                    </div>
+                // slice, maybe?
+                for (let i = fromIndex; i < toIndex; i++)
+                  items.push(
+                    <ClassCard
+                      key={i}
+                      row={index}
+                      courseData={props.courses[i]}
+                      active={props.active}
+                      openDetail={(course, row) => {
+                        props.openDetail(course, row);
+                        props.scrollTo(row);
+                      }}
+                    />
                   );
-                }}
-              />
-            </div>
-          );
-        }}
-      </AutoSizer>
-    </MainDiv>
-  );
-};
+                return (
+                  <div key={key} style={style}>
+                    {items}
+                  </div>
+                );
+              }}
+            />
+          </div>
+        );
+      }}
+    </AutoSizer>
+  </MainDiv>
+);
 
 export default Main;
