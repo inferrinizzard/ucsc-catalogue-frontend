f4223a65a4fe6f682ec4979c94b5fee49f6494e6
inferrinizzard <inferrinizzard@gmail.com>
2021-03-28 14:02:01 -0700

    fix webpack configs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,48 +4,52 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname + '/dist'),
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json', '.css'],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8000, // Convert images < 8kb to base64 strings
-              name: 'images/[hash]-[name].[ext]',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new FaviconsWebpackPlugin({
-      logo: path.join(__dirname, 'misc/UCSC_seal_logo.png')
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'public',
-      },
-    ]),
-  ],
+	mode: 'production',
+	entry: './src/index.tsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname + '/dist'),
+		publicPath: '/',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json', '.css'],
+	},
+	module: {
+		rules: [
+			{ test: /\.tsx?$/, loader: 'ts-loader' },
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+			},
+			{
+				test: /\.(png|jp(e*)g|svg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8000, // Convert images < 8kb to base64 strings
+							name: 'images/[hash]-[name].[ext]',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new FaviconsWebpackPlugin({
+			logo: path.join(__dirname, 'misc/UCSC_seal_logo.png'),
+		}),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src/index.html'),
+		}),
+		// new CopyWebpackPlugin([
+		// 	{
+		// 		patterns: [
+		// 			{
+		// 				from: 'public',
+		// 			},
+		// 		],
+		// 	},
+		// ]),
+	],
 };
