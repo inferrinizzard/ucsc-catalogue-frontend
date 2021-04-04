d34883daa769836cbb6d03cb2d8dfe0ba39953d2
inferrinizzard <inferrinizzard@gmail.com>
2021-04-04 16:07:18 -0700

    remove unused redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,20 @@
 import { createStore, combineReducers, Action, applyMiddleware } from 'redux';
-
-import UIReducer, { UIState, UIActions } from './UI';
 import CourseReducer, { CourseState, CourseActions, CourseEpics } from './course';
 import { combineEpics, createEpicMiddleware, EpicMiddleware } from 'redux-observable';
 import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
-	UI: UIReducer,
 	course: CourseReducer,
 });
 
 export type ReduxState = {
-	UI: UIState;
 	course: CourseState;
 };
 
 const rootEpic = combineEpics(CourseEpics);
 const epicMiddleware = createEpicMiddleware();
 
-export type ReduxAction = UIActions | CourseActions | Action;
+export type ReduxAction = CourseActions | Action;
 
 export const configureStore = () =>
 	createStore(rootReducer, applyMiddleware(epicMiddleware, logger));
--- src/store/tracking.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from 'redux';
-import * as APIModel from '../models/course.model';
-import API from '../services/api';
-
-export interface TrackingState {
-	courseEnrollment: APIModel.CourseEnrollment[];
-}
